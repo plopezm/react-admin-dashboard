88ad1921040e7ffa2d908d6ab1cd32510aa60e0b
pablolm <pabloplm@gmail.com>
2017-09-24 22:10:21 +0200

    Base

--- src/core/store.js
+++ src/core/store.js
@@ -1,6 +1,5 @@
 import {createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers/index'
-import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);

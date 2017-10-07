3d4406cb94da7b31c74def3bb028efd87178d84b
pablolm <pabloplm@gmail.com>
2017-10-07 16:10:19 +0200

    Fixes

--- src/components/form/ObjectFormulary.js
+++ src/components/form/ObjectFormulary.js
@@ -6,6 +6,12 @@ import ObjectCheckboxField from "./ObjectCheckboxField";
 
 
 class ObjectFormulary extends React.Component {
+    constructor(props){
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+    }
+
     onSubmit(e){
         e.preventDefault();
         if(this.props.onSubmit){
